219cd5c3b840c723b61e09d0a1cc32429e42a1c6
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-05-26 23:53:08 -0300

    Created Burger Class

--- src/br/com/eaugusto/builder/Burger.java
+++ src/br/com/eaugusto/builder/Burger.java
@@ -3,10 +3,58 @@
  * @since 26 de mai. de 2025
  */
 
-
 package br.com.eaugusto.builder;
 
 public class Burger {
 
-}
+	private String bun = "No Bun";
+	private String meat = "No Meat";
+	private String salad = "No Salad";
+	private String cheese = "No Cheese";
+	private String sauce = "No Sauce";
 
+	public String getBun() {
+		return bun;
+	}
+
+	public String getMeat() {
+		return meat;
+	}
+
+	public String getSalad() {
+		return salad;
+	}
+
+	public String getCheese() {
+		return cheese;
+	}
+
+	public String getSauce() {
+		return sauce;
+	}
+
+	public void setBun(String bun) {
+		this.bun = bun;
+	}
+
+	public void setMeat(String meat) {
+		this.meat = meat;
+	}
+
+	public void setSalad(String salad) {
+		this.salad = salad;
+	}
+
+	public void setCheese(String cheese) {
+		this.cheese = cheese;
+	}
+
+	public void setSauce(String sauce) {
+		this.sauce = sauce;
+	}
+
+	public void print() {
+		System.out.println("Burger is ready! \n" + "Bun: " + bun + " - Meat: " + meat + " - Cheese: " + cheese
+				+ " - Salad: " + salad + " - Sauce: " + sauce);
+	}
+}
--- src/br/com/eaugusto/builder/Demo.java
+++ src/br/com/eaugusto/builder/Demo.java
@@ -3,7 +3,6 @@
  * @since 26 de mai. de 2025
  */
 
-
 package br.com.eaugusto.builder;
 
 public class Demo {
@@ -12,9 +11,15 @@ public class Demo {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Burger burger = new Burger();
+
+		burger.setBun("Bun");
+		burger.setSauce("Sauce");
+		burger.setMeat("Meat");
+		burger.setCheese("Cheese");
+
+		burger.print();
 
 	}
 
 }
-
