6c8a906df76cb01c8da060f0a46976e27d348dee
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-05-29 02:46:01 -0300

    Created Car Abstract Class

---
+++ src/br/com/eaugusto/factorymethod/Car.java
@@ -0,0 +1,36 @@
+/**
+ * @author Eduardo Augusto (https://github.com/AsrielDreemurrGM/)
+ * @since 29 de mai. de 2025
+ */
+
+package br.com.eaugusto.factorymethod;
+
+public abstract class Car {
+
+	protected int horsePower;
+	protected String fuelSource;
+	protected String color;
+
+	protected Car(int horsePower, String fuelSource, String color) {
+		this.horsePower = horsePower;
+		this.fuelSource = fuelSource;
+		this.color = color;
+	}
+
+	public void startEngine() {
+		System.out.println("Car Model: " + getClass().getSimpleName());
+		System.out.println("Starting the " + fuelSource.toLowerCase() + " engine with " + horsePower + " HP.");
+	}
+
+	public void clean() {
+		System.out.println("Cleaning the car. The " + color.toLowerCase() + " paint shines!");
+	}
+
+	public void mechanicCheck() {
+		System.out.println("Performing mechanic check. Everything looks good!");
+	}
+
+	public void fuelCar() {
+		System.out.println("Filling the car with " + fuelSource.toLowerCase() + ".");
+	}
+}
