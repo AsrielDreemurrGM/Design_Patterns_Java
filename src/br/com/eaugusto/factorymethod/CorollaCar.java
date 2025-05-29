da9a9453c443c05dd6163604b050a7dfc2f35ebe
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-05-29 02:54:03 -0300

    Created Three Car Abstract Class Extentions
    - CorollaCar, JettaCar And ModelSCar;

---
+++ src/br/com/eaugusto/factorymethod/CorollaCar.java
@@ -0,0 +1,13 @@
+/**
+ * @author Eduardo Augusto (https://github.com/AsrielDreemurrGM/)
+ * @since 29 de mai. de 2025
+ */
+
+package br.com.eaugusto.factorymethod;
+
+public class CorollaCar extends Car {
+
+	protected CorollaCar(int horsePower, String fuelSource, String color) {
+		super(167, "Gasoline", "red");
+	}
+}
---
+++ src/br/com/eaugusto/factorymethod/JettaCar.java
@@ -0,0 +1,13 @@
+/**
+ * @author Eduardo Augusto (https://github.com/AsrielDreemurrGM/)
+ * @since 29 de mai. de 2025
+ */
+
+package br.com.eaugusto.factorymethod;
+
+public class JettaCar extends Car {
+
+	protected JettaCar(int horsePower, String fuelSource, String color) {
+		super(231, "Gasoline", "Blue");
+	}
+}
